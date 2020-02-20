71b41fde48a1e390019da15a8f8811d2d969c4b2
namiko1102 <bargman4@gmail.com>
2020-02-20 20:33:04 +0900

    rename application_name

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.12.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'chat-space'
+set :application, 'chat-space2'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:namiko1102/chat-space2.git'
